f2bbca655a014fe97d1fc784b7a40b9d39def586
tomaszpolanski <krigerek@gmail.com>
2015-07-06 17:18:45 +0200

    Changed hacky way of delaying elements in list

--- app/src/main/java/com/waygo/viewmodels/ChatViewModel.java
+++ app/src/main/java/com/waygo/viewmodels/ChatViewModel.java
@@ -13,12 +13,8 @@ import com.waygo.utils.Linq;
 import com.waygo.utils.ObservableEx;
 import com.waygo.utils.result.Result;
 
-import java.util.concurrent.TimeUnit;
-
 import rx.Observable;
-import rx.functions.Action1;
 import rx.subjects.BehaviorSubject;
-import rx.subjects.PublishSubject;
 import rx.subscriptions.CompositeSubscription;
 
 public class ChatViewModel extends AbstractViewModel {
@@ -31,7 +27,6 @@ public class ChatViewModel extends AbstractViewModel {
     private final IButler mButler;
     @NonNull
     private final ISchedulerProvider mSchedulerProvider;
-    private Linq<String> mQuestionsList = getQuestions();
 
     public ChatViewModel( @NonNull final IButler butler,
                           @NonNull final ISchedulerProvider schedulerProvider) {
@@ -51,18 +46,9 @@ public class ChatViewModel extends AbstractViewModel {
         return list;
     }
 
-    @NonNull
-    private String getQuestion() {
-        final String question = mQuestionsList.first().orDefault(() -> "");
-        mQuestionsList = mQuestionsList.skip(1);
-        return question;
-    }
-
     @Override
     protected void subscribeToDataStoreInternal(@NonNull CompositeSubscription subscriptions) {
-        final Observable<Person> questions =  Observable.interval(3, TimeUnit.SECONDS, mSchedulerProvider.getTimeScheduler())
-                .map(__ -> getQuestion())
-                .filter(a -> !a.isEmpty())
+        final Observable<Person> questions =  ObservableEx.delayEach(getQuestions(), 3, mSchedulerProvider.getTimeScheduler())
                 .map(sentence -> new User(sentence))
                 .ofType(Person.class)
                 .share();
