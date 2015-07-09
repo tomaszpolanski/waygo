3e7a2738aa89c9dc9dfa53aae1edd60c42b62115
tomaszpolanski <krigerek@gmail.com>
2015-07-09 07:07:44 +0200

    Improving option

--- app/src/main/java/com/waygo/utilskt/option/Option.kt
+++ app/src/main/java/com/waygo/utilskt/option/Option.kt
@@ -20,10 +20,12 @@ abstract class Option<T> {
 
     abstract public fun <OUT> map( selector:  (T) -> OUT ) : Option<OUT>
     abstract public fun <OUT> flatMap( selector:  (T) -> Option<OUT> ) : Option<OUT>
+    abstract public fun <OUT> ofType( type:  Class<OUT> ) : Option<OUT>
     abstract public fun filter( predicate:  (T) -> Boolean ) : Option<T>
     abstract public fun orOption( selector:  () -> Option<T> ) : Option<T>
     abstract public fun orDefault( selector:  () -> T ) : T
     abstract public fun toList() : List<T>
+    abstract public fun <OUT> match( some:  (T) -> OUT, none: () -> OUT) : OUT
 }
 
 class Some<T> internal constructor( val value : T) : Option<T>() {
@@ -34,10 +36,13 @@ class Some<T> internal constructor( val value : T) : Option<T>() {
 
     override fun <OUT> map( selector:  (T) -> OUT ) : Option<OUT> = Some(selector(value))
     override fun <OUT> flatMap(selector: (T) -> Option<OUT>): Option<OUT> = selector(value)
+    override fun <OUT> ofType(type: Class<OUT>): Option<OUT> = if (type.isInstance(value)) Some(value as OUT) else None()
     override fun filter(predicate: (T) -> Boolean): Option<T> = if (predicate(value)) this else None();
     override fun orOption(selector: () -> Option<T>): Option<T> = this
     override fun orDefault(selector: () -> T): T = value
     override fun toList(): List<T> = listOf(value)
+    override fun <OUT> match(some: (T) -> OUT, none: () -> OUT): OUT = some(value)
+
 }
 
 class None<T> internal constructor() : Option<T>() {
@@ -49,8 +54,10 @@ class None<T> internal constructor() : Option<T>() {
 
     override fun <OUT> map(selector: (T) -> OUT): Option<OUT> = None()
     override fun <OUT> flatMap(selector: (T) -> Option<OUT>): Option<OUT> = None()
+    override fun <OUT> ofType(type: Class<OUT>): Option<OUT>  = None()
     override fun filter(predicate: (T) -> Boolean): Option<T> = this
     override fun orOption(selector: () -> Option<T>): Option<T> = selector()
     override fun orDefault(selector: () -> T): T = selector()
     override fun toList(): List<T> = emptyList()
+    override fun <OUT> match(some: (T) -> OUT, none: () -> OUT): OUT = none()
 }
