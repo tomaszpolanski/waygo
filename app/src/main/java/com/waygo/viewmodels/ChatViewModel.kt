47efb876836df28b76663850a08285c25842070f
Tomasz Polanski <tomasz.polanski@futurice.com>
2015-07-12 14:15:17 +0200

    Changing viewmodel to kotlin

---
+++ app/src/main/java/com/waygo/viewmodels/ChatViewModel.kt
@@ -0,0 +1,48 @@
+package com.waygo.viewmodels
+
+
+import com.waygo.data.model.butler.ButlerResponse
+import com.waygo.data.model.conversation.Person
+import com.waygo.data.model.conversation.User
+import com.waygo.data.model.conversation.Waygo
+import com.waygo.data.provider.FakeButler
+import com.waygo.data.provider.interfaces.IButler
+import com.waygo.data.provider.interfaces.ISchedulerProvider
+import com.waygo.utils.Linq
+import com.waygo.utils.ObservableEx
+import com.waygo.utilskt.Option
+import com.waygo.utilskt.Result
+
+import rx.Observable
+import rx.functions.Func1
+import rx.subjects.BehaviorSubject
+import rx.subscriptions.CompositeSubscription
+
+public class ChatViewModel(private val mButler: IButler, private val mSchedulerProvider: ISchedulerProvider) : AbstractViewModel() {
+
+    private val mConversation = BehaviorSubject.create<Person>()
+
+    fun getQuestions() =
+            listOf(FakeButler.TOILETS, FakeButler.CHEERS, FakeButler.HUNGRY, FakeButler.CHEAPGOOD, FakeButler.MAP)
+
+    override fun subscribeToDataStoreInternal(subscriptions: CompositeSubscription) {
+        val questions = ObservableEx.delayEach(getQuestions(), 3, mSchedulerProvider.getTimeScheduler())
+                .map{User(it)}
+                .ofType(javaClass<Person>())
+                .share()
+
+        val answers = questions.switchMap{ObservableEx.chooseKt(mButler.ask(it.getSentence()), {op -> op.toOption()})}
+                .map{Waygo(it)}
+                .ofType(javaClass<Person>())
+
+        val conversation = questions.mergeWith(answers)
+                .startWith(Waygo("Ola, Jen. Welcome to Madrid."))
+
+        subscriptions.add(conversation.subscribe(mConversation))
+    }
+
+
+    public fun getConversation(): Observable<Person> {
+        return mConversation.asObservable()
+    }
+}
